9ab793ed6134afe94d47d870ad4f19eb5c7ec14e
Jacob Seiler <seiler.jacob1@gmail.com>
2020-04-13 12:56:38 -0400

    Merged JS into main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-$(document).ready(function () {
-	$(".color").colorpicker({
-		format: "rgba",
-		autoInputFallback: false,
-	});
-
-	$("#search-form").keypress(function (e) {
-		if (e.keyCode == 13) {
-			e.preventDefault();
-			return false;
-		}
-	});
-
-	document.addEventListener("focusin", function (e) {
-		if ($("#search").is(":focus")) filter();
-	});
-
-	filter();
-});
-
+// Search bar
 function filter() {
 	const filter = $("#search").val().toUpperCase();
 	const dropdown = $("#search-dropdown");
@@ -51,3 +32,127 @@ function filter() {
 function travel(element) {
 	window.location.href = element.href;
 }
+
+// Relative dates
+function relativeDates() {
+	const elements = $(".relative-date");
+	const today = new Date();
+
+	elements.each(function () {
+		const date = new Date($(this).text());
+		const days = Math.floor((today.getTime() - date.getTime()) / (1000 * 3600 * 24));
+
+		if (days < 0) {
+			$(this).addClass("disabled");
+
+			if (days == -1) $(this).text("Coming tomorrow");
+			else $(this).text("Coming in " + Math.abs(days) + " days");
+		} else {
+			if (days == 0) $(this).text("Added today");
+			else if (days === 1) $(this).text("Added yesterday");
+			else $(this).text("Added " + days + " days ago");
+		}
+	});
+}
+
+// Components & Examples
+function setCode(self) {
+	if (self.hasClass("component")) {
+		$("#code-paste").text("box-shadow: " + $(".box").css("box-shadow") + ";");
+	} else {
+		const box = self.parent().parent().children(".demo-example").children(".box-example");
+		const text = "box-shadow: " + box.css("box-shadow") + ";";
+		const paste = $("#code-paste").text(text);
+	}
+}
+
+function copyCode(self) {
+	let text;
+
+	if (self.hasClass("component")) {
+		text = "box-shadow: " + $(".box").css("box-shadow") + ";";
+	} else {
+		if ($("#code-modal").is(":visible")) {
+			text = self.parent().parent().children(".modal-body").children("#code-paste").text();
+		} else {
+			const box = self.parent().parent().children(".demo-example").children(".box-example");
+			text = "box-shadow: " + box.css("box-shadow") + ";";
+		}
+	}
+
+	const temp = document.createElement("textarea");
+	temp.value = text;
+
+	document.body.appendChild(temp);
+	temp.select();
+	temp.setSelectionRange(0, 99999);
+	temp.focus();
+	document.execCommand("copy");
+	document.body.removeChild(temp);
+}
+
+// Box Shadow
+function updateBoxShadow() {
+	const type = $(".form-btn:checked").parent().text().trim();
+	let style = "none";
+
+	if (type !== "None") {
+		const color = $("#form-color").val();
+		const x = $("#position-h").val();
+		const y = $("#position-v").val();
+		const blur = $("#radius-blur").val();
+		const spread = $("#radius-spread").val();
+
+		style = x + "px " + y + "px " + blur + "px " + spread + "px " + color;
+
+		if (type === "Inset") style += " inset";
+	}
+
+	$(".box").css("box-shadow", style);
+}
+
+$(document).ready(function () {
+	// Setup color pickers
+	$(".color").colorpicker({
+		format: "rgba",
+		autoInputFallback: false,
+	});
+
+	// Search bar
+	$("#search-form").keypress(function (e) {
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			return false;
+		}
+	});
+
+	document.addEventListener("focusin", function (e) {
+		if ($("#search").is(":focus")) filter();
+	});
+
+	// Popover
+	$(".popover-dismiss").popover({
+		trigger: "focus",
+	});
+	$('[data-toggle="popover"]').popover();
+
+	// Components & Examples
+	$(".btn-view").click(function () {
+		setCode($(this));
+	});
+
+	$(".btn-copy").on("shown.bs.popover", function () {
+		copyCode($(this));
+	});
+
+	// Box Shadow
+	$(".update-boxshadow").each(function () {
+		if ($(this).hasClass("form-btn")) $(this).click(updateBoxShadow);
+		else $(this).change(updateBoxShadow);
+	});
+
+	// Call functions to setup
+	filter();
+	relativeDates();
+	updateBoxShadow();
+});
