a3fd0719488e35fc23b152c36d09bb1420dd33c8
Jacob Seiler <seiler.jacob1@gmail.com>
2020-04-14 23:04:15 -0400

    Added support for multiple box shadows

--- js/components.js
+++ js/components.js
@@ -15,3 +15,104 @@ function copyCode(property) {
 	document.execCommand("copy");
 	document.body.removeChild(temp);
 }
+
+function addTab() {
+	const header = $(".tabs").children(".nav-tabs");
+
+	const newTab = document.createElement("LI");
+	newTab.classList.add("nav-item");
+
+	const newTabContent = document.createElement("A");
+	newTabContent.classList.add("nav-link");
+	newTabContent.setAttribute("id", "tab-0");
+	newTabContent.setAttribute("data-toggle", "tab");
+	newTabContent.setAttribute("href", "#tab-content-0");
+	newTabContent.setAttribute("role", "tab");
+	newTabContent.setAttribute("aria-controls", "tab-content-0");
+	newTabContent.setAttribute("aria-selected", "false");
+	newTabContent.innerHTML = "0";
+
+	newTab.appendChild(newTabContent);
+	header.append(newTab);
+
+	const body = $(".tabs").children(".tab-content");
+	const text = body.children().last().text();
+
+	const newBody = document.createElement("DIV");
+	newBody.classList.add("tab-pane");
+	newBody.classList.add("fade");
+	newBody.setAttribute("id", "tab-content-0");
+	newBody.setAttribute("role", "tabpanel");
+	newBody.setAttribute("aria-labelledby", "tab-0");
+	newBody.innerHTML = text;
+	body.append(newBody);
+
+	setActiveTab(-1);
+	cleanTabs();
+
+	// Unhide tab bar
+	header.removeClass("d-none");
+}
+
+function removeTab(tabnum) {
+	// Check if not last tab
+
+	$(".tabs")
+		.children(".nav-tabs")
+		.children(".nav-item")
+		.children("#tab-" + tabnum)
+		.parent()
+		.remove();
+
+	$(".tabs")
+		.children(".tab-content")
+		.children("#tab-content-" + tabnum)
+		.remove();
+
+	cleanTabs();
+	setActiveTab(tabnum);
+
+	// If only one tab, hide
+	if ($(".tabs").children(".nav-tabs").children().length < 2) $(".tabs").children(".nav-tabs").addClass("d-none");
+}
+
+function renameTab(tabnumOld, tabnumNew) {
+	if (tabnumOld === tabnumNew) return;
+
+	const header = $(".tabs")
+		.children(".nav-tabs")
+		.children(".nav-item")
+		.children("#tab-" + tabnumOld);
+
+	header.attr("id", "tab-" + tabnumNew);
+	header.attr("href", "#tab-content-" + tabnumNew);
+	header.attr("aria-controls", "tab-content-" + tabnumNew);
+	header.text(tabnumNew);
+
+	const body = $(".tabs")
+		.children(".tab-content")
+		.children("#tab-content-" + tabnumOld);
+	body.attr("id", "tab-content-" + tabnumNew);
+	body.attr("aria-labelledby", "tab-" + tabnumNew);
+}
+
+function setActiveTab(tabnum) {
+	if (tabnum < 1 || tabnum > $(".tabs .nav-tabs").children().length) $(".tabs .nav-tabs li:last-child a").tab("show");
+	else $(".tabs .nav-tabs li:nth-child(" + tabnum + ") a").tab("show");
+}
+
+function cleanTabs() {
+	const header = $(".tabs").children(".nav-tabs").children(".nav-item");
+
+	for (var i = 0; i < header.length; i++) {
+		const tabnum = parseInt(header[i].children[0].id.split("-")[1]);
+
+		if (tabnum !== i + 1) renameTab(tabnum, i + 1);
+	}
+
+	if (!header.children("a").hasClass("active")) setActiveTab(header.length);
+
+	// Disable delete button when only 1 tab
+	if (header.length < 2) $(".btn-del").addClass("disabled");
+	else $(".btn-del").removeClass("disabled");
+}
