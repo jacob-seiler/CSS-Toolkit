0b89a4fec37d7eae9c04f7fcb3502cc8c452f7ed
Jacob Seiler <seiler.jacob1@gmail.com>
2020-11-03 15:34:09 -0500

    Added layout to design page

--- src/pages/Design.tsx
+++ src/pages/Design.tsx
@@ -1,16 +1,42 @@
 import React from "react";
 import Button from "react-bootstrap/esm/Button";
+import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
+import Jumbotron from "react-bootstrap/esm/Jumbotron";
+import Row from "react-bootstrap/esm/Row";
 import { Link } from "react-router-dom";
 
 const Design: React.FC = () => {
 	return (
-		<Container>
-			<h1 className="display-1">Design</h1>
-			<Link to="/about">
-				<Button>Go to about</Button>
-			</Link>
-		</Container>
+		<>
+			<Jumbotron>
+				<Container>
+					<h1 className="display-1">Design</h1>
+					<Link to="/about">
+						<Button>Go to about</Button>
+					</Link>
+				</Container>
+			</Jumbotron>
+			<Container>
+				<Row>
+					<Col>
+						<p>Controls</p>
+					</Col>
+					<Col>
+						<Row>
+							<Col>
+								<p>Display</p>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<p>Code</p>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 };
 
