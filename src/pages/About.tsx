8f3024e06d73d0d25c16e710958ed09e6e33a566
Jacob Seiler <seiler.jacob1@gmail.com>
2020-11-03 15:29:02 -0500

    Added some stuff to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import Button from "react-bootstrap/esm/Button";
 import Container from "react-bootstrap/esm/Container";
+import Jumbotron from "react-bootstrap/esm/Jumbotron";
 import { Link } from "react-router-dom";
 
 const About: React.FC = () => {
 	return (
-		<Container>
-			<h1 className="display-1">About</h1>
-			<Link to="/">
-				<Button>Go home</Button>
-			</Link>
-		</Container>
+		<>
+			<Jumbotron fluid>
+				<Container>
+					<h1 className="display-1">About</h1>
+					<Link to="/">
+						<Button variant="primary">Go home</Button>
+					</Link>
+				</Container>
+			</Jumbotron>
+			<Container>
+				<p className="lead">
+					CSS Toolkit is a web application for designing and styling CSS components using
+					interactive tools.
+				</p>
+				<p>Info about CSS</p>
+				<p>contact us info</p>
+			</Container>
+		</>
 	);
 };
 
